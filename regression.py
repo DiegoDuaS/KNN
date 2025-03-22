56feeb14088ec9ff2ed655d65fae904aa0a5d476
MarchMol <marchmol98@gmail.com>
2025-03-21 19:33:35 -0600

    feat: tuned KNN

--- regression.py
+++ regression.py
@@ -1,5 +1,6 @@
 import scipy.stats as stats
 import numpy as np
+from sklearn.metrics import mean_absolute_error, mean_squared_error, root_mean_squared_error, r2_score
 
 def drop_many_nulls(df):
     df = df.copy()  # Evitar modificar el dataframe original
@@ -15,3 +16,14 @@ def drop_many_nulls(df):
     df = df.drop(columns=drop_columns, errors='ignore')
     
     return df
+
+def performance_metrics(y,y_pred):
+    rmse = root_mean_squared_error(y, y_pred)
+    mae_knn = mean_absolute_error(y, y_pred)
+    mse_knn = mean_squared_error(y, y_pred)
+    r2 = r2_score(y, y_pred)
+
+    print(f"RMSE en test: {rmse}")
+    print(f"MAE en test: {mae_knn}")
+    print(f"MSE en test: {mse_knn}")
+    print(f"R² en test: {r2}")
